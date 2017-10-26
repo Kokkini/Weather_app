cd425f67c634e3fda22b7bab678a08bd8b024246
Kokkini <trannhatquang1104@gmail.com>
2017-10-25 22:51:49 -0400

    parse json file

--- local_main.py
+++ local_main.py
@@ -1,7 +1,18 @@
 import requests
+import datetime
 
-zipcode = input("Zip Code: ")
-url = 'http://api.openweathermap.org/data/2.5/weather?zip='+str(zipcode)+',us&appid=3aa596c4010eb1744783beac955c8e54'
-json_data = requests.get(url).json()
+url = 'http://api.openweathermap.org/data/2.5/weather?appid=3aa596c4010eb1744783beac955c8e54&q=Philadelphia,us'
+data = requests.get(url).json()
 
-print(json_data)
+zeroC = 273.15
+
+print "At the moment, in", data['name']
+print
+print "Temperature:", data['main']['temp']-zeroC,"C"
+print "Humidity   :", data['main']['humidity'],"%"
+print "Wind speed :", data['wind']['speed']*3.6,"km/h"
+print
+print data['weather'][0]['description']
+print
+print "Sunrise:",datetime.datetime.fromtimestamp(data['sys']['sunrise'])
+print "Sunset :",datetime.datetime.fromtimestamp(data['sys']['sunset'])
